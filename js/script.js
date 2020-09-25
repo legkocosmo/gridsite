729aa41e1915f21f6a509f912493cd102bb9f6f6
legkocosmo <mylegko@gmail.com>
2020-09-25 11:52:36 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-jQuery(function($) {
+jQuery(function() {
     $('.header__burger').click(function (e) { 
       e.preventDefault();
       $('.header__burger, .header__menu').toggleClass('active');
